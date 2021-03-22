5469c935257a53628b5ff7606ed80bd1c41208a3
Jacob Hampton <jhampton792@gmail.com>
2021-03-22 17:48:00 -0500

    commit tree 67117f63aa59efd09d8ed0b08345b9673b701aff

--- src/App.js
+++ src/App.js
@@ -1,1 +1,33 @@
-// create your App component here
+import { render } from 'enzyme'
+import React from 'react'
+
+export default class App extends React.Component {
+
+    constructor() {
+        super()
+
+        this.state = {
+            peopleInSpace: []
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.peopleInSpace.map(person => {
+                    return <h2>{person.name}</h2>
+                })}
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+            .then(res => res.json())
+            .then(data => {
+                this.setState({
+                    peopleInSpace: data.people
+                })
+            })
+    }
+}
